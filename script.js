37be005bd99642bb13022a1928a08abecd423db3
jakesummer <jakersummer@gmail.com>
2026-04-08 16:50:13 -0400

    Plan out project

---
+++ script.js
@@ -0,0 +1,19 @@
+/* Player Object (factory function)
+Takes player's name
+Variable for number of games won
+Returns getter for games won, and function to add 1 to amount of games won */
+
+
+/* Gameboard Object (IIFE) 
+Store gameboard in 2d array (each grid starts out as null and gets updated to X or O depending on players icon)
+gameboard getter
+Update grid function 
+Print gameboard function 
+Reset gameboard function*/
+
+/* Game manager object (IIFE) 
+Play round function
+Get current player function
+Change current player function 
+Check winner function
+Reset game function */
